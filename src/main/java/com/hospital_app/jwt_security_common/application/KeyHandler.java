b3bc7e15554acd88dc2007e697bf9b7f57b22625
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-08-25 20:03:32 -0300

    fix: Removing private key responsibility

--- src/main/java/com/hospital_app/jwt_security_common/application/KeyHandler.java
+++ src/main/java/com/hospital_app/jwt_security_common/application/KeyHandler.java
@@ -6,10 +6,8 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -20,13 +18,7 @@ public class KeyHandler {
         return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(spec);
     }
 
-    public static RSAPrivateKey getPrivateKey(Resource key) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(getFileBytes(key));
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        return (RSAPrivateKey) kf.generatePrivate(spec);
-    }
-
-    private static byte[] getFileBytes(Resource key) throws IOException {
+    public static byte[] getFileBytes(Resource key) throws IOException {
         String keyContent = new String(key.getInputStream().readAllBytes())
                 .replace("-----BEGIN PUBLIC KEY-----", "")
                 .replace("-----END PUBLIC KEY-----", "")
